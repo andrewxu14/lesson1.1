8acb4d52d643d3ca6c51d6ceda3e2ab26c79fbbc
AX <xua7482@my.sduhsd.net>
2024-08-19 11:08:07 -0700

    dasdf

---
+++ class Main{.java
@@ -0,0 +1,22 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println("First and Last Name: Andrew Xu");
+        System.out.println("Class Period: 3");
+        System.out.println("My favorite thing to do: Eat Food");
+        System.out.println("Why you took this class: I like coding");
+        System.out.println("'\\              .  .                           |>18>>");
+        System.out.println("\\            .         ' .                     |");
+        System.out.println("O>>        .                 'o                |");
+        System.out.println("\\       .                                      |");
+        System.out.println("/\\    .                                        |");
+        System.out.println("/ /  .'                                        |");
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    }
+}
+
+
+
+
+
+
+
